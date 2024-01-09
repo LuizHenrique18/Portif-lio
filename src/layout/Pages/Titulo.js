4df5be44b5c5bb96309f14d33e3f26551c1fe404
LuizHenrique18 <luizlopespereira10@gmail.com>
2024-01-08 22:56:26 -0300

    Update

--- src/layout/Pages/Titulo.js
+++ src/layout/Pages/Titulo.js
@@ -9,8 +9,8 @@ function Titulo() {
         <div className={style.slide1}>
             <div className={style.divImage}>
                 <div className={style.divLeft}>
-                    <h2 className={style.h2Titulo}>Olá, Seja Bem-vindo ao meu porfólio!</h2>
-                    <Link className={style.buttao}> Entre </Link>
+                    <h2 className={style.h2Titulo}>MEU NOME É LUIZ HENRIQUE, SEJA BEM-VINDO AO MEU PORTIFÓLIO!</h2>
+                    <Link className={style.buttao}> VER MAIS </Link>
                 </div>
                 <div className={style.divRight}>
                     <div className={style.divMyImage}>
